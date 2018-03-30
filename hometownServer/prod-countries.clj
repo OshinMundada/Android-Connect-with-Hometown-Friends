d8cd2c76e7b59caf15a808c2df81fda0013956db
OshinMundada <mundadaoshin@gmail.com>
2018-03-30 13:56:09 -0700

    server files containing countries, states, users

---
+++ hometownServer/prod-countries.clj
@@ -0,0 +1,18 @@
+[ {:country "Canada"
+   :states [ "Ontario", "Quebec", "Nova Scotia", "New Brunswick", "Manitoba", "British Columbia", "Prince Edward Island", "Saskatchewan", "Alberta", "Newfoundland and Labrador", "St. John's"]}
+ {:country "China"
+   :states ["Beijing Municipality", "Tianjin Municipality", "Hebei", "Shanxi", "Inner Mongolia", "Liaoning", "Jilin", "Heilongjiang", "Shanghai Municipality", "Jiangsu", "Zhejiang", "Anhui", "Fujian", "Jiangxi", "Shandong", "Henan", "Hubei", "Hunan", "Guangdong", "Guangxi Zhuang", "Hainan", "Chongqing Municipality", "Sichuan", "Guizhou", "Yunnan", "Tibet Autonomous Region", "Shaanxi", "Gansu", "Qinghai", "Ningxia Hui", "Xinjiang Uyghur", "Hong Kong", "Macau", "Taiwan"]}
+ {:country "France"
+  :states [ "Grand-Est", "New Aquitaine", "Auvergne-Rhône-Alpes", "Burgundy-Franche-Comté", "Brittany", "Centre-Val de Loir", "Île-de-France", "Occitania", "Hauts-de-France", "Normandy", "Pays de la Loire", "Provence-Alpes-Côte d'Azur", "Corsica"]}
+ {:country "Germany"
+  :states [ "Baden-Württemberg", "Bavaria", "Berlin", "Brandenburg", "Bremen", "Hamburg", "Hesse", "Lower Saxony", "Mecklenburg-Vorpommern", "North Rhine-Westphalia", "Rhineland-Palatinate", "Saarland", "Saxony", "Saxony-Anhalt", "Schleswig-Holstein", "Thuringia"]}
+ {:country "India"
+  :states ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal"]}
+ {:country "Japan"
+  :states [ "Aichi", "Akita", "Aomori", "Chiba", "Ehime", "Fukui", "Fukuoka", "Fukushima", "Gifu", "Gunma", "Hiroshima", "Hokkaido", "Hyōgo", "Ibaraki", "Ishikawa", "Iwate", "Kagawa", "Kagoshima", "Kanagawa", "Kōchi", "Kumamoto", "Kyoto", "Mie", "Miyagi", "Miyazaki", "Nagano", "Nagasaki", "Nara", "Niigata", "Ōita", "Okayama", "Okinawa", "Osaka", "Saga", "Saitama", "Shiga", "Shimane", "Shizuoka", "Tochigi", "Tokushima", "Tokyo", "Tottori", "Toyama", "Wakayama", "Yamagata", "Yamaguchi", "Yamanashi"]}
+ {:country "Mexico"
+  :states ["Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas", "Chihuahua", "Coahuil", "Colim", "Durango", "Guanajuato", "Guerrero", "Hidalgo", "Jalisco", "México", "Michoacán", "Morelos", "Nayarit", "Nuevo Leó", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí", "Sinaloa", "Sonor", "Tabasc", "Tamaulipa", "Tlaxcala", "Veracruz", "Yucatá", "Zacatecas"]}
+ {:country "USA"
+  :states ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]}
+
+ ]
---
+++ hometownServer/prod-locations.clj
@@ -0,0 +1,6 @@
+{"California" {:latitude 36.794471 :longitude -119.336696}
+ "Nevada" {:latitude 39.523670 :longitude -116.787868}
+ "Arizona" {:latitude 34.510612 :longitude -111.691620}
+ "Oregon" {:latitude 44.298890 :longitude -120.621499}
+ "USA" {:latitude 39.967233 :longitude -96.847090}
+ }
